c159e698331f30594a26abece521bddedf7d773c
dieezs <diegomagasouza@gmail.com>
2020-06-27 20:06:34 -0300

    done main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -23,6 +23,9 @@ import { FiSend as Transferencia } from 'react-icons/fi'
 import { IoMdHelp as Suporte } from 'react-icons/io'
 import { FaMoneyBillAlt as Cambio } from 'react-icons/fa'
 
+import { AiOutlineRise as Investimento } from 'react-icons/ai'
+import { GiFoodTruck as Restaurante } from 'react-icons/gi'
+
 const Main = () => (
     <div className="Main">
 
@@ -61,7 +64,7 @@ const Main = () => (
                 </li>
                 <hr />
                 <li>
-                    <Investimentos size={30} className="icon" />
+                    <Investimento size={30} className="icon" />
                     <p className="textList">Investimentos</p>
                 </li>
                 <hr />
@@ -134,6 +137,77 @@ const Main = () => (
                     <br />
                     <Cambio size={40} />
                     <p>Câmbio</p>
+                </div>
+
+            </div>
+            <br /><br />
+            <div className="content">
+                <div className="investimentos">
+                    <p className="title">Investimentos</p>
+                    <p className="tinyTitle">saldo</p>
+                    <p className="saldo">R$ <span>64.672,32</span></p>
+                    <br />
+                    <div className="investimentosResumo">
+
+                        <div className="item">
+                            <p className="tinyTitle">Fundos de Investimento</p>
+                            <p className="saldo">R$ <span>22.402,68</span></p>
+                        </div>
+
+                        <div className="item">
+                            <p className="tinyTitle">Previdência Privada</p>
+                            <p className="saldo">R$ <span>4.142,02</span></p>
+                        </div>
+
+                        <div className="item">
+                            <p className="tinyTitle">Fundos Imobiliários</p>
+                            <p className="saldo">R$ <span>2.722,75</span></p>
+                        </div>
+
+                        <div className="item">
+                            <p className="tinyTitle">Ações</p>
+                            <p className="saldo">R$ <span>13.202,85</span></p>
+                        </div>
+
+                    </div>
+                </div>
+                <br /><br />
+                <div className="contacorrente">
+                    <p className="title">Uso do cartão de crédito</p>
+                    <div className="status">
+                        <div className="item">
+                            <p className="tinyTitle">Fatura atual</p>
+                            <p className="saldo">R$ <span>542,67</span></p>
+                        </div>
+
+                        <div className="item">
+                            <p className="tinyTitle">Limite disponível</p>
+                            <p className="saldo">R$ <span>2.142,32</span></p>
+                        </div>
+
+                    </div>
+                    <br />
+                    <p className="ultimasCompras">Ultimas compras</p>
+                    <div className="compras">
+                        <div className="item">
+                            <p className="titleCompra"> Restaurante - McDonalds</p>
+                            <p className="titlePrice"> no valor de R$ <span>15,90</span></p>
+                            <p>15/06</p>
+                        </div>
+
+                        <div className="item">
+                            <p className="titleCompra"> Mercado - Extra</p>
+                            <p className="titlePrice"> no valor de R$ <span>152,22</span></p>
+                            <p>13/06</p>
+                        </div>
+
+                        <div className="item">
+                            <p className="titleCompra"> Estética - Barbershop</p>
+                            <p className="titlePrice"> no valor de R$ <span>25,50</span></p>
+                            <p>09/06</p>
+                        </div>
+
+                    </div>
                 </div>
 
             </div>
